c48e60f4c6a83b641dd0fab33049565eef6c7da5
lordzzz <lordzz@gmail.com>
2024-05-29 18:28:03 +0200

    Día 48, Paul Hudson día 26,  Proyecto 4, primera parte

---
+++ 100 Dias SwiftUI Paul Hudson/Proyectos/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Esteban Perez Castillejo on 29/5/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
