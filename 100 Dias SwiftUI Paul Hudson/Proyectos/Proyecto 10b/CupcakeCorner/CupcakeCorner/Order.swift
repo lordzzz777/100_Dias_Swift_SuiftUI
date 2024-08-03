4301c8fb4f485b95f6d18ecd2bc148f4f9a4ea78
lordzzz <lordzz@gmail.com>
2024-08-03 20:14:23 +0200

    Día 72, Paul Hudson día  51,  Proyecto 10, tercera parte

--- 100 Dias SwiftUI Paul Hudson/Proyectos/Proyecto 10b/CupcakeCorner/CupcakeCorner/Order.swift
+++ 100 Dias SwiftUI Paul Hudson/Proyectos/Proyecto 10b/CupcakeCorner/CupcakeCorner/Order.swift
@@ -6,9 +6,23 @@
 //
 
 import Foundation
+import Observation
 
 @Observable
-class Order {
+class Order: Codable {
+    
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnabled = "specialRequestEnabled"
+        case _extraFrosting = "extraFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _city = "city"
+        case _streetAddress = "streetAddress"
+        case _zip = "zip"
+    }
+    
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
 
     var type = 0
@@ -23,6 +37,39 @@ class Order {
         }
     }
     
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+
+        return true
+    }
+    
+    var cost: Decimal {
+        // $2 per cake
+        var cost = Decimal(quantity) * 2
+
+        // complicated cakes cost more
+        cost += Decimal(type) / 2
+
+        // $1/cake for extra frosting
+        if extraFrosting {
+            cost += Decimal(quantity)
+        }
+
+        // $0.50/cake for sprinkles
+        if addSprinkles {
+            cost += Decimal(quantity) / 2
+        }
+
+        return cost
+    }
     var extraFrosting = false
     var addSprinkles = false
+    
+   
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
 }
