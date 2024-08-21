859dc24d1a39d0c5cd0e279af6c77c306d264869
lordzzz <lordzz@gmail.com>
2024-08-21 09:20:40 +0200

    Día 87, Paul Hudson día  66,  Proyecto 13, quinta parte

--- 100 Dias SwiftUI Paul Hudson/Proyectos/Proyecto 13/Instafilter/Instafilter/ContentView.swift
+++ 100 Dias SwiftUI Paul Hudson/Proyectos/Proyecto 13/Instafilter/Instafilter/ContentView.swift
@@ -13,14 +13,19 @@ import PhotosUI
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var ratio = 0.5
     @State private var selectedItem: PhotosPickerItem?
-    @State private var currentFilter = CIFilter.sepiaTone()
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    @State private var showingFilters = false
+    
+    @AppStorage("filterCount") var filterCount = 0
+    @Environment(\.requestReview) var requestReview
     
     let context = CIContext()
     
     var body: some View {
         NavigationStack{
-            VStack {
+            VStack(alignment: .leading) {
                 Spacer()
                 
                 PhotosPicker(selection: $selectedItem){
@@ -36,20 +41,40 @@ struct ContentView: View {
                 Spacer()
                 
                 HStack{
-                     Text("Intensity")
+                    Text("Intensity: ")
                     Slider(value: $filterIntensity)
                         .onChange(of: filterIntensity, applyProcessing)
+                }.disabled((selectedItem != nil) ? false : true)
+                HStack{
+                    Text("Radio:      ")
+                    Slider(value: $ratio)
+                        .onChange(of: ratio, applyProcessing)
+                }.disabled((selectedItem != nil) ? false : true)
                     
                     // Cambiar Filtro
                     Button("Change Filter", action: changeFilter)
-                }
+                    .disabled((selectedItem != nil) ? false : true)
+
+              
                 Spacer()
                 
-                // ... Busacador de fotos
+                if let processedImage {
+                    ShareLink(item: processedImage, preview: SharePreview("Instafilter image", image: processedImage))
+                }
                 
             }
             .padding([.horizontal, .bottom])
             .navigationTitle("Instafilter")
+            .confirmationDialog("Select a filter", isPresented: $showingFilters) {
+                Button("Crystallize") { setFilter(CIFilter.crystallize()) }
+                Button("Edges") { setFilter(CIFilter.edges()) }
+                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
+                Button("Pixellate") { setFilter(CIFilter.pixellate()) }
+                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
+                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
+                Button("Vignette") { setFilter(CIFilter.vignette()) }
+                Button("Cancel", role: .cancel) { }
+            }
         }
     }
     
@@ -65,7 +90,12 @@ struct ContentView: View {
     }
     
     func applyProcessing() {
-        currentFilter.intensity = Float(filterIntensity)
+        //  currentFilter.intensity = Float(filterIntensity)
+        let inputKeys = currentFilter.inputKeys
+
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
 
         guard let outputImage = currentFilter.outputImage else { return }
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
@@ -75,7 +105,19 @@ struct ContentView: View {
         
     }
     
+    @MainActor func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
+        
+        filterCount += 1
+
+        if filterCount >= 20 {
+            requestReview()
+        }
+    }
+    
     func changeFilter() {
+        showingFilters = true
     }
     
 }
