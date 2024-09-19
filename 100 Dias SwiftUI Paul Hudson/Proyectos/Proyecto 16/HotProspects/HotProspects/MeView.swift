083e03cab824b8487f15fc5b571010aa1698dea6
lordzzz <lordzzz8@gmail.com>
2024-09-19 21:56:43 +0200

    Día 105, Paul Hudson día  84, Proyecto 16, Sexta parte

--- 100 Dias SwiftUI Paul Hudson/Proyectos/Proyecto 16/HotProspects/HotProspects/MeView.swift
+++ 100 Dias SwiftUI Paul Hudson/Proyectos/Proyecto 16/HotProspects/HotProspects/MeView.swift
@@ -12,6 +12,8 @@ struct MeView: View {
     @AppStorage("name") private var name = "Anonymous"
     @AppStorage("emailAddress") private var emailAddress = "you@yoursite.com"
     
+    @State private var qrCode = UIImage()
+    
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
     
@@ -26,13 +28,21 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .resizable()
                     .interpolation(.none)
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                       
+
+                        ShareLink(item: Image(uiImage: qrCode), preview: SharePreview("My QR Code", image: Image(uiImage: qrCode)))
+                    }
             }
             .navigationTitle("Your code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name, updateCode)
+            .onChange(of: emailAddress, updateCode)
         }
     }
     
@@ -40,12 +50,16 @@ struct MeView: View {
         filter.message = Data(string.utf8)
 
         if let outputImage = filter.outputImage {
-            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
-                return UIImage(cgImage: cgImage)
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
             }
         }
 
         return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+    
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
 }
 
--- 100 Dias SwiftUI Paul Hudson/Proyectos/Proyecto 16/HotProspects/HotProspects/ProspectsView.swift
+++ 100 Dias SwiftUI Paul Hudson/Proyectos/Proyecto 16/HotProspects/HotProspects/ProspectsView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import SwiftData
 import CodeScanner
+import UserNotifications
 
 struct ProspectsView: View {
     @State private var isShowingScanner = false
@@ -30,27 +31,7 @@ struct ProspectsView: View {
         }
     }
     
-    func handleScan(result: Result<ScanResult, ScanError>) {
-        isShowingScanner = false
-        
-        switch result {
-        case .success(let result):
-            let details = result.string.components(separatedBy: "\n")
-            guard details.count == 2 else { return }
-
-            let person = Prospect(name: details[0], emailAddress: details[1], isContacted: false)
-
-            modelContext.insert(person)
-        case .failure(let error):
-            print("Scanning failed: \(error.localizedDescription)")
-        }
-    }
     
-    func delete() {
-        for prospect in selectedProspects {
-            modelContext.delete(prospect)
-        }
-    }
     
     @Query(sort: \Prospect.name) var prospects: [Prospect]
     @Environment(\.modelContext) var modelContext
@@ -91,7 +72,12 @@ struct ProspectsView: View {
                             prospect.isContacted.toggle()
                         }
                         .tint(.green)
+                        
                     }
+                    Button("Remind Me", systemImage: "bell") {
+                        addNotification(for: prospect)
+                    }
+                    .tint(.orange)
                 }
                 .tag(prospect)
             }
@@ -115,6 +101,60 @@ struct ProspectsView: View {
             }
             .sheet(isPresented: $isShowingScanner) {
                 CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: handleScan)
+            }
+        }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+            let person = Prospect(name: details[0], emailAddress: details[1], isContacted: false)
+            
+            modelContext.insert(person)
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
+        }
+    }
+    
+    func delete() {
+        for prospect in selectedProspects {
+            modelContext.delete(prospect)
+        }
+    }
+    
+    func addNotification(for prospect: Prospect) {
+        let center = UNUserNotificationCenter.current()
+
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.emailAddress
+            content.sound = UNNotificationSound.default
+
+            var dateComponents = DateComponents()
+            dateComponents.hour = 9
+          //  let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        }
+
+        center.getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        addRequest()
+                    } else if let error {
+                        print(error.localizedDescription)
+                    }
+                }
             }
         }
     }
