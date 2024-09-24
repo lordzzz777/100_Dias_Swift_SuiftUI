b298d8f279a0c3dd93c8ce62fa7f6416e994402b
lordzzz <lordzzz8@gmail.com>
2024-09-24 21:55:57 +0200

    Día 109, Paul Hudson día  88, Proyecto 17, tercera parte

---
+++ 100 Dias SwiftUI Paul Hudson/Proyectos/Proyecto 17/Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Esteban Pérez Castillejo on 24/9/24.
+//
+
+import SwiftUI
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
--- 100 Dias SwiftUI Paul Hudson/Proyectos/Proyecto 17/Flashzilla/Flashzilla/ContentView.swift
+++ 100 Dias SwiftUI Paul Hudson/Proyectos/Proyecto 17/Flashzilla/Flashzilla/ContentView.swift
@@ -8,17 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]){
+                            withAnimation{
+                                removeCard(at: index)
+                            }
+                        }
+                            .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
+    }
 }
