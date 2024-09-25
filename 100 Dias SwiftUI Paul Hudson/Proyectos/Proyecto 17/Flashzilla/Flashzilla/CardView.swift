3a97c4dc68621ef7acdf6b049bc16fba15318d55
lordzzz <lordzzz8@gmail.com>
2024-09-25 10:02:23 +0200

    Día 110, Paul Hudson día  89, Proyecto 17, cuarta parte

--- 100 Dias SwiftUI Paul Hudson/Proyectos/Proyecto 17/Flashzilla/Flashzilla/CardView.swift
+++ 100 Dias SwiftUI Paul Hudson/Proyectos/Proyecto 17/Flashzilla/Flashzilla/CardView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct CardView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+    
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
     
@@ -19,7 +21,19 @@ struct CardView: View {
         
         ZStack {
             RoundedRectangle(cornerRadius: 25)
-                .fill(.white)
+                .fill(
+                    accessibilityDifferentiateWithoutColor
+                        ? .white
+                        : .white
+                            .opacity(1 - Double(abs(offset.width / 50)))
+
+                )
+                .background(
+                    accessibilityDifferentiateWithoutColor
+                        ? nil
+                        : RoundedRectangle(cornerRadius: 25)
+                            .fill(offset.width > 0 ? .green : .red)
+                )
                 .shadow(radius: 10)
             VStack {
                 Text(card.prompt)
