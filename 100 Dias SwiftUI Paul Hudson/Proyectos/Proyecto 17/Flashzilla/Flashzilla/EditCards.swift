406cc2d0037da81ef072f54d52921310e06921c3
lordzzz <lordzzz8@gmail.com>
2024-09-25 23:21:00 +0200

    Día 111, Paul Hudson día  90, Proyecto 17, quinta parte

---
+++ 100 Dias SwiftUI Paul Hudson/Proyectos/Proyecto 17/Flashzilla/Flashzilla/EditCards.swift
@@ -0,0 +1,18 @@
+//
+//  EditCards.swift
+//  Flashzilla
+//
+//  Created by Esteban Pérez Castillejo on 25/9/24.
+//
+
+import SwiftUI
+
+struct EditCards: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditCards()
+}
